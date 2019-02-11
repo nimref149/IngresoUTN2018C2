cf94482eac66c9b977e98568b2c431a864de0835
nimref149 <47321357+nimref149@users.noreply.github.com>
2019-02-11 12:37:01 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,17 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarDescuento()
 {
+    var importe;
+    var descuento;
+    var precioFinal;
+    importe = parseFloat(document.getElementById("importe").value);
+    
+    descuento = importe + 25 / 100;
+   
+    precioFinal = importe - descuento;
+    
+    document.getElementById("resultado").value = precioFinal;
+    
+    alert("Se aplico un descuento de $" + descuento);                                                                                       ")
 	
 }
